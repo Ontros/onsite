dbda884108a5cd75d5539e29b5136bcc0a5af073
Ontros <ontro512@gmail.com>
2022-06-30 22:00:49 +0200

    Database schema

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,17 @@
 import NextAuth from "next-auth"
 import DiscordProvider from "next-auth/providers/discord"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
 
 if (!process.env.DISCORD_CLIENT_ID || !process.env.DISCORD_CLIENT_SECRET) {
     throw new Error("Missing env [...nextauth].ts");
 }
 
+const prisma = new PrismaClient()
+
 export default NextAuth({
     // Configure one or more authentication providers
+    adapter: PrismaAdapter(prisma),
     providers: [
         DiscordProvider({
             clientId: process.env.DISCORD_CLIENT_ID,
